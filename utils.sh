a5da320a6367b1a1f04652b9dc301902e3b2e35d
krvstek. <138402769+krvstek@users.noreply.github.com>
2026-02-06 15:19:42 +0000

    chore: little fix for apkm errors

--- utils.sh
+++ utils.sh
@@ -181,7 +181,7 @@ _req() {
 		mv -f "$dlp" "$op"
 	fi
 }
-req() { _req "$1" "$2" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:146.0) Gecko/20100101 Firefox/146.0"; }
+req() { _req "$1" "$2" --tls-max 1.2 --http1.1 -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:147.0) Gecko/20100101 Firefox/147.0"; }
 gh_req() { _req "$1" "$2" -H "$GH_HEADER"; }
 gh_dl() {
 	if [ ! -f "$1" ]; then
